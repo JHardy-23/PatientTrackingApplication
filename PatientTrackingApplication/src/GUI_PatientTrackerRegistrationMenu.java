dc1d19a89628deb5b7e4fef1560d7577df87c4d0
anushka-vadde <anushka-vadde@users.noreply.github.com>
2021-11-10 20:13:58 -0600

    Added Registration Menu

---
+++ PatientTrackingApplication/src/GUI_PatientTrackerRegistrationMenu.java
@@ -0,0 +1,41 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class GUI_PatientTrackerRegistrationMenu {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GUI_PatientTrackerRegistrationMenu window = new GUI_PatientTrackerRegistrationMenu();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public GUI_PatientTrackerRegistrationMenu() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 800, 650);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
