06b14c1dbc47c7c29675146f0aceb049b6a998c8
anushka-vadde <anushka-vadde@users.noreply.github.com>
2021-11-10 20:06:14 -0600

    Added Labels and Buttons on Homepage(Starter Page)

--- PatientTrackingApplication/src/PatientTrackerHomepageGUI.java
+++ PatientTrackingApplication/src/PatientTrackerHomepageGUI.java
@@ -1,6 +1,12 @@
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.SwingConstants;
 
 public class PatientTrackerHomepageGUI {
 
@@ -34,9 +40,30 @@ public class PatientTrackerHomepageGUI {
 	 */
 	private void initialize() {
 		frame = new JFrame();
+		frame.getContentPane().setFont(new Font("Tahoma", Font.PLAIN, 27));
 		frame.setBounds(100, 100, 800, 650);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
+		
+		JLabel HomepageTitle = new JLabel("Patient Tracker");
+		HomepageTitle.setFont(new Font("Algerian", Font.PLAIN, 60));
+		HomepageTitle.setBounds(110, 28, 585, 91);
+		frame.getContentPane().add(HomepageTitle);
+		
+		JButton PatientButton = new JButton("Sign In");
+		PatientButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		PatientButton.setBounds(469, 469, 197, 41);
+		frame.getContentPane().add(PatientButton);
+		
+		JButton RegisterButton = new JButton("Register");
+		RegisterButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		RegisterButton.setBounds(102, 469, 197, 41);
+		frame.getContentPane().add(RegisterButton);
 	}
-
 }
