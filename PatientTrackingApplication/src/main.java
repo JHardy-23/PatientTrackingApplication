4c94465b1341e52d8aef7504024feaf3c5601f4c
cmn180003 <81604577+cmn180003@users.noreply.github.com>
2021-11-22 22:42:45 -0600

    Created LoginGUI files, main.java, and a package for images including our logo. Feel free to change everything about main.java.

---
+++ PatientTrackingApplication/src/main.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+       
+
+
+/**
+ *
+ * @author cmn68
+ */
+public class main extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        try{
+        Parent root = FXMLLoader.load(getClass().getResources("LoginGUI.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
