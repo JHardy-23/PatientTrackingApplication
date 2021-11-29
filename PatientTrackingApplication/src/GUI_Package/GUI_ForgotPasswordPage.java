61214f817c5e365b1bd4551505ba9d830b812d34
anushka-vadde <anushka-vadde@users.noreply.github.com>
2021-11-28 22:27:43 -0600

    Updated GUI_ForgotPasswordPage

--- PatientTrackingApplication/src/GUI_Package/GUI_ForgotPasswordPage.java
+++ PatientTrackingApplication/src/GUI_Package/GUI_ForgotPasswordPage.java
@@ -3,10 +3,16 @@ package GUI_Package;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
 public class GUI_ForgotPasswordPage {
 
 	private JFrame frame;
+	private JTextField emailAddresstextField;
 
 	/**
 	 * Launch the application.
@@ -36,8 +42,34 @@ public class GUI_ForgotPasswordPage {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 1142, 420);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel ForgotPassTitleLabel = new JLabel("Forgot Password?");
+		ForgotPassTitleLabel.setFont(new Font("Algerian", Font.PLAIN, 40));
+		ForgotPassTitleLabel.setBounds(373, 28, 378, 33);
+		frame.getContentPane().add(ForgotPassTitleLabel);
+		
+		JLabel informationLabel = new JLabel("Enter the email associated with your account and we'll send an email with instructions to reset your password");
+		informationLabel.setVerticalAlignment(SwingConstants.TOP);
+		informationLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		informationLabel.setFont(new Font("Tahoma", Font.PLAIN, 21));
+		informationLabel.setBounds(26, 85, 1058, 40);
+		frame.getContentPane().add(informationLabel);
+		
+		emailAddresstextField = new JTextField();
+		emailAddresstextField.setBounds(322, 168, 429, 39);
+		frame.getContentPane().add(emailAddresstextField);
+		emailAddresstextField.setColumns(10);
+		
+		JLabel lblNewLabel_2 = new JLabel("Email Address");
+		lblNewLabel_2.setBounds(322, 126, 185, 33);
+		frame.getContentPane().add(lblNewLabel_2);
+		
+		JButton submitButton = new JButton("Submit ");
+		submitButton.setBounds(454, 235, 171, 41);
+		frame.getContentPane().add(submitButton);
 	}
 
 }
