bc9a06b68adc02ffd6c33a4a962e4e3b364c8a64
anushka-vadde <anushka-vadde@users.noreply.github.com>
2021-11-28 18:22:24 -0600

    created Appointments Page with skeleton code

---
+++ PatientTrackingApplication/src/GUI_Package/GUI_DoctorAppointmentsPage.java
@@ -0,0 +1,46 @@
+package GUI_Package;
+
+import java.awt.EventQueue;
+import java.awt.Font;
+
+import javax.swing.JFrame;
+
+public class GUI_DoctorAppointmentsPage {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GUI_DoctorAppointmentsPage window = new GUI_DoctorAppointmentsPage();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public GUI_DoctorAppointmentsPage() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.getContentPane().setFont(new Font("Tahoma", Font.PLAIN, 27));
+		frame.setBounds(100, 100, 800, 650);
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+	}
+
+}
