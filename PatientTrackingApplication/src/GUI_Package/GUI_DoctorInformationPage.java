87a10e646f47a0e05d2bfdb7066da35f1aa1e843
anushka-vadde <anushka-vadde@users.noreply.github.com>
2021-11-30 14:44:44 -0600

    Created Doctor Information Page GUI

---
+++ PatientTrackingApplication/src/GUI_Package/GUI_DoctorInformationPage.java
@@ -0,0 +1,132 @@
+package GUI_Package;
+
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+
+public class GUI_DoctorInformationPage {
+
+	private JFrame frame;
+	private JTextField suffixTextField;
+	private JTextField firstNametectField;
+	private JTextField lastNameTextField;
+	private JTextField professionTextField;
+	private JTextField educationTextField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GUI_DoctorInformationPage window = new GUI_DoctorInformationPage();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public GUI_DoctorInformationPage() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.getContentPane().setFont(new Font("Tahoma", Font.PLAIN, 27));
+		frame.setBounds(100, 100, 775, 632);
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel informationLabel = new JLabel("Information");
+		informationLabel.setFont(new Font("Algerian", Font.PLAIN, 35));
+		informationLabel.setBounds(260, 28, 264, 33);
+		frame.getContentPane().add(informationLabel);
+		
+		
+		
+		JLabel firstNameLabel = new JLabel("First Name:");
+		firstNameLabel.setBounds(49, 127, 177, 33);
+		frame.getContentPane().add(firstNameLabel);
+		
+		JLabel lastNameLabel = new JLabel("Last Name:");
+		lastNameLabel.setBounds(49, 184, 177, 33);
+		frame.getContentPane().add(lastNameLabel);
+		
+		
+		JLabel professionLabel = new JLabel("Profession:");
+		professionLabel.setHorizontalAlignment(SwingConstants.LEFT);
+		professionLabel.setBounds(49, 245, 218, 33);
+		frame.getContentPane().add(professionLabel);
+		
+		
+		JLabel PatientsLabel = new JLabel("Patients:");
+		PatientsLabel.setBounds(49, 306, 177, 33);
+		frame.getContentPane().add(PatientsLabel);
+		
+		JButton PatientsButton = new JButton("Click here for Patients");
+		PatientsButton.setFont(new Font("Tahoma", Font.PLAIN, 17));
+		PatientsButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		PatientsButton.setBounds(384, 306, 236, 41);
+		frame.getContentPane().add(PatientsButton);
+		
+		JButton editButton = new JButton("Edit");
+		editButton.setBounds(270, 475, 171, 41);
+		frame.getContentPane().add(editButton);
+		
+		JLabel educationLabel = new JLabel("Education:");
+		educationLabel.setBounds(49, 367, 226, 33);
+		frame.getContentPane().add(educationLabel);
+		
+		JLabel SuffixLabel = new JLabel("Suffix :");
+		SuffixLabel.setHorizontalAlignment(SwingConstants.LEFT);
+		SuffixLabel.setBounds(49, 76, 115, 33);
+		frame.getContentPane().add(SuffixLabel);
+		
+		suffixTextField = new JTextField();
+		suffixTextField.setBounds(384, 73, 236, 39);
+		frame.getContentPane().add(suffixTextField);
+		suffixTextField.setColumns(10);
+		
+		firstNametectField = new JTextField();
+		firstNametectField.setBounds(384, 124, 236, 39);
+		frame.getContentPane().add(firstNametectField);
+		firstNametectField.setColumns(10);
+		
+		lastNameTextField = new JTextField();
+		lastNameTextField.setBounds(384, 181, 236, 39);
+		frame.getContentPane().add(lastNameTextField);
+		lastNameTextField.setColumns(10);
+		
+		professionTextField = new JTextField();
+		professionTextField.setBounds(384, 242, 236, 39);
+		frame.getContentPane().add(professionTextField);
+		professionTextField.setColumns(10);
+		
+		educationTextField = new JTextField();
+		educationTextField.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		educationTextField.setBounds(384, 368, 283, 95);
+		frame.getContentPane().add(educationTextField);
+		educationTextField.setColumns(10);
+		
+	}
+}
