87a8a285114a4e429e5d8a5b5809a1032ad6949f
nickctomlin <nickctomlin@gmail.com>
2021-12-01 15:22:02 -0600

    Doctor Info finished

--- PatientTrackingApplication/src/GUI_Package/GUI_DoctorInformationPage.java
+++ PatientTrackingApplication/src/GUI_Package/GUI_DoctorInformationPage.java
@@ -5,21 +5,24 @@ import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.Component;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
 public class GUI_DoctorInformationPage {
 
-	private JFrame frame;
-	private JTextField suffixTextField;
-	private JTextField firstNametectField;
-	private JTextField lastNameTextField;
-	private JTextField professionTextField;
-	private JTextField educationTextField;
+	public JFrame frame;
+	public JTextField suffixTextField;
+	public JTextField firstNametectField;
+	public JTextField lastNameTextField;
+	public JTextField professionTextField;
+	public JTextField educationTextField;
+	public JTextArea textArea;
 
 	/**
 	 * Launch the application.
@@ -106,27 +109,54 @@ public class GUI_DoctorInformationPage {
 		suffixTextField.setBounds(384, 73, 236, 39);
 		frame.getContentPane().add(suffixTextField);
 		suffixTextField.setColumns(10);
+		suffixTextField.setEditable(false);
+		suffixTextField.setBackground(Color.WHITE);
 		
 		firstNametectField = new JTextField();
 		firstNametectField.setBounds(384, 124, 236, 39);
 		frame.getContentPane().add(firstNametectField);
 		firstNametectField.setColumns(10);
-		
+		firstNametectField.setEditable(false);
+		firstNametectField.setBackground(Color.WHITE);
+
 		lastNameTextField = new JTextField();
 		lastNameTextField.setBounds(384, 181, 236, 39);
 		frame.getContentPane().add(lastNameTextField);
 		lastNameTextField.setColumns(10);
-		
+		lastNameTextField.setEditable(false);
+		lastNameTextField.setBackground(Color.WHITE);
+
 		professionTextField = new JTextField();
 		professionTextField.setBounds(384, 242, 236, 39);
 		frame.getContentPane().add(professionTextField);
 		professionTextField.setColumns(10);
+		professionTextField.setEditable(false);
+		professionTextField.setBackground(Color.WHITE);
 		
+		/*
 		educationTextField = new JTextField();
 		educationTextField.setFont(new Font("Tahoma", Font.PLAIN, 18));
 		educationTextField.setBounds(384, 368, 283, 95);
 		frame.getContentPane().add(educationTextField);
 		educationTextField.setColumns(10);
-		
+		educationTextField.setEditable(false);
+		educationTextField.setBackground(Color.WHITE);*/
+
+		textArea = new JTextArea(2 , 20);
+		textArea.setBounds(384,368,283,95);
+		frame.getContentPane().add(textArea);
+		textArea.setEditable(false);
+		textArea.setWrapStyleWord(true);
+		textArea.setLineWrap(true);
+
+	}
+
+	public JButton getButton(String text) {
+		for(Component c : frame.getContentPane().getComponents()) {
+			if( c instanceof JButton && ((JButton) c).getText().equals(text) ) {
+				return (JButton)c;
+			}
+		}
+		return null;
 	}
 }
